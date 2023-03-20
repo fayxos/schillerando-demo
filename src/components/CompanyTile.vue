596ab8ed9c7019154ada8d893e6ab6a7f5cde769
jucktnich <73582096+jucktnich@users.noreply.github.com>
2023-03-20 16:05:34 +0100

    Create

---
+++ src/components/CompanyTile.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container-fluid">
+      <p>{{ companyObject.name }}</p>
+      <p v-for="category in companyObject.categories" :key="category">{{ category }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'CompanyTile',
+    props: ["companyObject"]
+  }
+  </script>
