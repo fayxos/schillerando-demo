763729c0c1d48a520ce0a0de6506731446ff1227
jucktnich <73582096+jucktnich@users.noreply.github.com>
2023-03-20 16:05:04 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://jwfyrkfpqfkzovzdpkqt.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3Znlya2ZwcWZrem92emRwa3F0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzkyNDg4MjcsImV4cCI6MTk5NDgyNDgyN30.ECsAuWRN08LWBqJZqa4v3Qmnx03N8wkGrTRgVMotCS8"
+export const supabase = createClient(supabaseUrl, supabaseKey)
+
 createApp(App)
     .use(router)
     .mount('#app')
