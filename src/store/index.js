2c01f246e3903e67b12bfc7bb6d050f7d0d4045e
Felix Haag <64311838+fayxos@users.noreply.github.com>
2023-05-11 23:15:20 +0200

    Store

--- src/store/index.js
+++ src/store/index.js
@@ -1,94 +1,261 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView';
-import ProductView from '../views/ProductView';
-import CompanyView from '../views/CompanyView';
-import CompanyDetailView from '../views/CompanyDetailView';
-import AccountView from '../views/AccountView';
-import AuthView from '../views/AuthView';
-import UpdatePasswordView from '../views/UpdatePasswordView';
-import AGBView from '../views/AGBView';
-import store from '../store/index';
+import { createStore } from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
+import router from '../router';
+import { supabase } from '../supabase';
 
-const routes = [
-  {
-    path: '/',
-    name: 'HomeView',
-    component: HomeView,
+const store = createStore({
+  state: {
+    user: null,
+    session: null,
+    state: undefined,
+    shoppingCart: [],
   },
-  {
-    path: '/produkte',
-    name: 'ProductView',
-    component: ProductView,
-  },
-  {
-    path: '/unternehmen',
-    name: 'CompanyView',
-    component: CompanyView,
-  },
-  {
-    path: '/account',
-    name: 'AccountView',
-    component: AccountView,
-    meta: {
-      requiresAuth: true,
+  mutations: {
+    setUser(state, payload) {
+      state.user = payload;
+    },
+    setState(state, payload) {
+      state.state = payload;
+    },
+    addProductToCart(state, payload) {
+      state.shoppingCart.push(payload);
+    },
+    removeProductToCart(state, payload) {
+      state.shoppingCart.filter((product) => product.id != payload.id);
     },
   },
-  {
-    path: '/auth',
-    name: 'AuthView',
-    component: AuthView,
-    meta: {
-      footer: false,
+  getters: {
+    getUser(state) {
+      return state.user;
+    },
+    getState(state) {
+      return state.state;
+    },
+    getProductsInCart(state) {
+      return state.shoppingCart;
     },
   },
-  {
-    path: '/update-password',
-    name: 'UpdatePasswordView',
-    component: UpdatePasswordView,
-    meta: {
-      footer: false,
+  actions: {
+    async reload({ commit }) {
+      const { data, error } = await supabase.auth.refreshSession();
+
+      if (error || data.session == null) {
+        commit('setUser', null);
+        this.dispatch('getSharedLogin');
+      } else {
+        commit('setUser', data.user);
+
+        this.dispatch('updateMissingMetadata');
+        this.dispatch('checkUserCompany');
+      }
+
+      this.timer = setInterval(() => {
+        this.dispatch('getSharedLogin');
+      }, 1000);
+    },
+    async getSharedLogin({ commit }) {
+      const cookies = document.cookie
+        .split(/\s*;\s*/)
+        .map((cookie) => cookie.split('='));
+      const accessTokenCookie = cookies.find(
+        (x) => x[0] == 'supabase-access-token'
+      );
+      const refreshTokenCookie = cookies.find(
+        (x) => x[0] == 'supabase-refresh-token'
+      );
+      if (accessTokenCookie && refreshTokenCookie) {
+        if (this.getters.getUser != null) return;
+        const { data, error } = await supabase.auth.setSession({
+          access_token: accessTokenCookie[1],
+          refresh_token: refreshTokenCookie[1],
+        });
+
+        if (error || data.session == null) {
+          commit('setUser', null);
+          router.go(router.currentRoute);
+        } else {
+          commit('setUser', data.user);
+
+          this.dispatch('updateMissingMetadata');
+          this.dispatch('checkUserCompany');
+
+          router.go(router.currentRoute);
+        }
+      } else if (this.getters.getUser != null) {
+        commit('setUser', null);
+        router.go(router.currentRoute);
+      } else {
+        commit('setUser', null);
+      }
+    },
+    async updateMissingMetadata({ commit }) {
+      if (this.getters.getUser.user_metadata.credit == null) {
+        const { data } = await supabase.auth.updateUser({
+          data: { credit: 0 },
+        });
+        commit('setUser', data.user);
+      }
+      if (this.getters.getUser.user_metadata.credit == null) {
+        const { data } = await supabase.auth.updateUser({
+          data: { isCompanyLeader: false },
+        });
+        commit('setUser', data.user);
+      }
+    },
+    async signInAction({ commit }, { form, path }) {
+      try {
+        commit('setState', 'loading');
+
+        const { data, error } = await supabase.auth.signInWithPassword({
+          email: form.email,
+          password: form.password,
+        });
+        if (error) throw error;
+        console.log('Successfully signed in');
+        commit('setUser', data.user);
+        this.dispatch('checkUserCompany');
+
+        commit('setState', 'success');
+
+        if (path == null) await router.replace('/account');
+        else if (path.split('_')[0] == 'ext') {
+          window.location.replace(
+            process.env.VUE_APP_BUSINESS_URL + path.split('_')[1] + '?ext=true'
+          );
+        } else await router.replace(path);
+      } catch (error) {
+        commit('setState', 'failure');
+        console.log(error.error_description || error.message);
+      }
+    },
+
+    async signUpAction({ commit }, { form, path }) {
+      try {
+        commit('setState', 'loading');
+
+        const splitName = form.name.split(' ');
+        const name1 =
+          splitName[0].charAt(0).toUpperCase() +
+          splitName[0].slice(1).toLowerCase();
+        const name2 =
+          splitName[1].charAt(0).toUpperCase() +
+          splitName[1].slice(1).toLowerCase();
+
+        const capitalizedName = name1 + ' ' + name2;
+
+        const { data, error } = await supabase.auth.signUp({
+          email: form.email,
+          password: form.password,
+          options: {
+            data: {
+              name: capitalizedName,
+              credit: 0,
+              isCompanyLeader: false,
+            },
+          },
+        });
+        if (error) throw error;
+        console.log('Successfully registered');
+        commit('setUser', data.user);
+
+        commit('setState', 'success');
+
+        this.dispatch('checkUserCompany');
+
+        if (path == null) await router.replace('/account');
+        else await router.replace(path);
+      } catch (error) {
+        commit('setState', 'failure');
+        console.log(error.error_description || error.message);
+      }
+    },
+
+    async signOutAction({ commit }) {
+      try {
+        commit('setState', 'loading');
+
+        const { error } = await supabase.auth.signOut();
+        if (error) throw error;
+        commit('setUser', null);
+        console.log('Logged Out successfully');
+
+        commit('setState', 'success');
+
+        await router.replace('/auth');
+      } catch (error) {
+        commit('setState', 'failure');
+        console.log(error.error_description || error.message);
+      }
+    },
+
+    async resetPasswordAction({ commit }, form) {
+      try {
+        commit('setState', 'loading');
+
+        const { error } = await supabase.auth.resetPasswordForEmail(
+          form.email,
+          {
+            redirectTo: process.env.VUE_APP_MAIN_URL + '/update-password',
+          }
+        );
+        if (error) throw error;
+
+        commit('setState', 'success');
+      } catch (error) {
+        commit('setState', 'failure');
+        console.log(error.error_description || error.message);
+      }
+    },
+    async updatePasswordAction({ commit }, form) {
+      try {
+        commit('setState', 'loading');
+
+        const { error } = await supabase.auth.updateUser({
+          password: form.password,
+        });
+        if (error) throw error;
+
+        commit('setState', 'success');
+
+        await router.replace('/account');
+      } catch (error) {
+        commit('setState', 'failure');
+        console.log(error.error_description || error.message);
+      }
+    },
+
+    async checkUserCompany({ commit }) {
+      try {
+        const { data, error } = await supabase
+          .from('companies')
+          .select()
+          .or(
+            'user_uid.eq.' +
+              this.getters.getUser.id +
+              ',employees.cs.' +
+              '{"' +
+              this.getters.getUser.email +
+              '"}'
+          );
+
+        if (error) throw error;
+
+        let user = this.getters.getUser;
+        user.hasCompany = false;
+        commit('setUser', user);
+        if (data[0] == null) return;
+
+        user.hasCompany = true;
+        commit('setUser', user);
+      } catch (error) {
+        console.log(error.error_description || error.message);
+      }
     },
   },
-  {
-    path: '/agb',
-    name: 'AGBView',
-    component: AGBView,
-  },
-  {
-    path: '/qr1',
-    redirect: '/',
-  },
-  {
-    path: '/qr2',
-    redirect: '/account',
-  },
-  {
-    path: '/:companyalias',
-    component: CompanyDetailView
-  }
-];
 
+  modules: {},
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-  scrollBehavior() {
-    document.getElementById('app').scrollIntoView({ behavior: 'smooth' });
-    document.getElementById('navbarToggler').classList.remove('show');
-  },
+  plugins: [createPersistedState()],
 });
 
-router.beforeEach((to, from, next) => {
-  // get current user info
-  const user = store.getters.getUser;
-  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
-  
-  if (requiresAuth && user == null)
-    next({ path: 'auth', query: { redirect: to.fullPath } });
-  else if (to.name == 'AuthView' && user != null) next({ path: 'account' });
-  else if (!requiresAuth && user != null) next();
-  else next();
-});
-
-
-export default router;
+export default store;
